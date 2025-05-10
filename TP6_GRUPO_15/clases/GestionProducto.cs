b28455527b07788e5c92357571e5cfba3de5ad36
NicolasPiccioni <Nicolas_Piccioni@hotmail.com>
2025-05-10 19:05:19 -0300

    EJ01: Se agregó clase "GestionProductos"

---
+++ TP6_GRUPO_15/clases/GestionProducto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TP6_GRUPO_15.clases
+{
+    public class GestionProducto
+    {
+
+        // CONSTRUCTORES
+        public GestionProducto()
+        {
+
+        }
+
+        public GestionProducto(int ID, string NombreProducto, string Cant_X_Unidad, decimal Precio_X_Unidad)
+        {
+         
+        }
+    }
+}
