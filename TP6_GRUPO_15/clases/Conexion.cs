60647f035e080b7824e7f71711712bcf33598455
fran-bk <frantigre2000@gmail.com>
2025-05-10 16:24:55 -0300

    Confección "Clase conexión" y colocar enlace a la Base de Datos "Neptuno".

---
+++ TP6_GRUPO_15/clases/Conexion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace TP6_GRUPO_15.clases
+{
+    public class Conexion
+    {
+        private const string cadenaConexion = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
+        
+        public SqlConnection ObtenerConexion()
+        {
+            SqlConnection conexion = new SqlConnection(cadenaConexion);
+            conexion.Open();
+            return conexion;
+        }
+
+        public void CerrarConexion(SqlConnection conexion)
+        {
+            if (conexion != null && conexion.State == System.Data.ConnectionState.Open)
+            {
+                conexion.Close();
+            }
+        }
+    }
+}
